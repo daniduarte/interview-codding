5bf5b1e04f6ccfdaa9472d2c01316d90e0bf8d81
Daniel Duarte <dduarte1267@gmail.com>
2018-12-27 15:49:39 -0300

    Adding logic for print products updates by days

--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 'use strict';
 
 const Product = require('./classes/Product');
-const product1 = new Product('nombre', 10, 100);
+const CarInsurance = require('./classes/CarInsurance');
 
-console.log('product1 name', product1.getName());
-console.log('product1 sellIn', product1.getSellIn());
-console.log('product1 price', product1.getPrice());
+const carInsurance = new CarInsurance;
+
+carInsurance.addProduct(new Product('Medium Coverage', 10, 20));
+carInsurance.addProduct(new Product('Full Coverage', 2, 0));
+carInsurance.addProduct(new Product('Low Coverage', 5, 7));
+carInsurance.addProduct(new Product('Mega Coverage', 0, 80));
+carInsurance.addProduct(new Product('Mega Coverage', -1, 80));
+carInsurance.addProduct(new Product('Special Full Coverage', 15, 20));
+carInsurance.addProduct(new Product('Special Full Coverage', 10, 49));
+carInsurance.addProduct(new Product('Special Full Coverage', 5, 49));
+carInsurance.addProduct(new Product('Super Sale', 3, 6));
+
+const productPrinter = function (product) {
+  console.log(`${product.getName()}, ${product.getSellIn()}, ${product.getPrice()}`);
+};
+
+console.log('OMGHAI!');
+
+
+
+for (let i = 0; i <= 30; i += 1) {
+  console.log(`-------- day ${i} --------`);
+  console.log('name, sellIn, price');
+  carInsurance.getProducts().map(function(coverage){
+    console.log(`${coverage.getName()}, ${coverage.getSellIn()}, ${coverage.getPrice()}`);
+  });
+  carInsurance.updatePrice();
+  console.log('');
+}
+
