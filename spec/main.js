21ecb984edc83339c739025eb08fcb08c5dd645d
Daniel Duarte <dduarte1267@gmail.com>
2018-12-27 13:41:38 -0300

    Adding spec for Product class

---
+++ spec/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const chai = require('chai');
+const assert = chai.assert;
+const Product = require('../src/classes/Product');
+
+describe('Classes validations', function () {
+
+  let productName = 'Producto';
+  let productSellIn = 10;
+  let productPrice = 100;
+  let product = new Product(productName, 10, 100);;
+
+  describe('Person', function () {
+
+    it ('Should valid class', function () {
+      assert.typeOf(product, 'object');
+    });
+
+    it ('Should return name', function () {
+      assert.equal(product.getName(), productName);
+    });
+
+    it ('Should return sellIn', function () {
+      assert.equal(product.getSellIn(), productSellIn);
+    });
+
+    it ('Should return price', function () {
+      assert.equal(product.getPrice(), productPrice);
+    });
+
+  })
+});
+
